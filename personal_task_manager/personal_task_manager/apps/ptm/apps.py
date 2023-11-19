4cafb8a8d2b9e59e18ffa483fe83029af5d44ad6
ronik-v <andronnikovvv@gmail.com>
2023-11-19 14:14:03 +0300

    ptm-initial

---
+++ personal_task_manager/personal_task_manager/apps/ptm/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class PtmConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ptm'
+    verbose_name = 'Personal task manager'
---
+++ personal_task_manager/personal_task_manager/apps/ptm/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+
+# views app pages
+from .views import (
+    TaskList,
+    TaskDetail,
+    TaskCreate,
+    TaskUpdate,
+    DeleteView,
+    CustomLoginView,
+    RegisterPage,
+    TaskReorder
+)
+
+urlpatterns = [
+    # user authentication
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
+
+    # task manager
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
+    path('task-reorder/', TaskReorder.as_view(), name='task-reorder'),
+]
